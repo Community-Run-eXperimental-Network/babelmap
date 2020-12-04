61469a675e2db5e046e68bf6d87df38318767fb0
Tristan B. Kildaire <deavmi@disroot.org>
2020-12-05 00:00:36 +0200

    Initial code commit

---
+++ mapper.py
@@ -0,0 +1,85 @@
+class Route:
+	def __init__(self, network, via, moi):
+		self.network = network
+		self.via = via
+		self.moi = moi
+
+	def getRoute(self):
+		return self.network
+
+	def getVia(self):
+		return self.via
+
+	def getMoi(self):
+		return self.moi
+
+	def __str__(self):
+		return "route: [Network: "+self.network+", Via: " + self.via+"]"
+
+def getRoutes(babelArray, myID):
+	routes = []
+	
+	for line in babelArray:
+		elements = line.split(" ")
+		
+		if elements[0] == "add" and elements[1] == "route":
+			routes.append(Route(elements[4], elements[10], myID))
+
+	return routes
+
+
+def babelCollect(ip, port):
+	import subprocess
+	babelData = subprocess.getoutput("bash -c \"echo dump | ncat "+ip+" "+port+" --no-shutdown -i 1\"")
+
+	"Only valid ncat one would have this (as the connection succeeded and we got a babel daemon response)"
+	if "my-id" in babelData:
+		babelArray = babelData.split("\n")
+		babelID = babelArray[3].split(" ")[1]
+		print("My ID: "+babelID)
+
+		routes = getRoutes(babelArray, babelID)
+		return routes
+
+	return None
+
+def collectAllRoutes(peersFile):
+	file = open(peersFile, "r")
+	data = file.read()
+
+	allRoutes = []
+
+	for peer in  data.split("\n"):
+		nodeIP = peer.split(" ")[0]
+		nodePort = peer.split(" ")[1]
+
+		routes = babelCollect(nodeIP, nodePort)
+		if routes != None:
+			for route in routes:
+				allRoutes.append(route)
+
+	return allRoutes
+
+def makeGraph(peersFile):
+	routes = collectAllRoutes(peersFile)
+	print("Collected "+str(len(routes))+" routes")
+
+	graph="graph networkMap {"
+
+	"Yeeet"
+	#graph+="graph [pad=\"5\", nodesep=\"0.1\", ranksep=\"10\"]"
+
+	for route in routes:
+		graph += "\""+route.getMoi()+"\" -- \""+route.getVia() +"\" [label=\""+route.getRoute()+"\"]"
+
+	graph+="}"
+
+	"Write out graph to disk"
+	file = open("thing.dot","w")
+	file.write(graph)
+	file.close()
+
+	import os
+	os.system("cat thing.dot | dot -Tsvg -o graph.svg")
+
+makeGraph("peers.list")
